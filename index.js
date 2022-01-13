94585a42d72947596832f041571811dbcd35e7c1
Zwischenbrugger Bernhard <A6102@pad01.aeat.allianz.at>
2022-01-13 12:32:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { Entsoe } = require('express-entsoe');
+const swaggerUi = require('swagger-ui-express');
+
+
+const app = express()
+const port = 3300
+
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(null, {
+  swaggerOptions: {
+    url: '/entsoe'
+  }
+}));
+
+
+app.use(Entsoe.init({
+  securityToken: '68aa46a3-3b1b-4071-ac6b-4372830b114f',
+  cacheDir: 'cacheDir'
+}));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
